51bc491d5bfd812d10378e8f1d1ad09f52249f4b
Matheus Nunes <matheusnunesinfo@gmail.com>
2025-06-28 18:48:39 -0300

    Error handler

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,8 +1,33 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 import { env } from './env'
 
 const server = fastify({})
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, _request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      statusCode: 400,
+      error: 'Validation Error',
+      message: error.validation,
+    })
+  }
+
+  console.error(error)
+  return reply.status(500).send({
+    statusCode: 500,
+    error: 'Internal Server Error',
+    message: 'An unexpected error occurred',
+  })
+})
 
 server.register(fastifyCors, {
   origin: '*',
