7b49a66992c2b14d39ebf0c20e12f9975a60f63b
Matheus Nunes <matheusnunesinfo@gmail.com>
2025-06-28 15:27:35 -0300

    Setup docker and env vars with zod

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,5 +1,6 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import { env } from './env'
 
 const server = fastify({})
 
@@ -9,6 +10,8 @@ server.register(fastifyCors, {
   allowedHeaders: ['Content-Type', 'Authorization'],
 })
 
-server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log('Server is running on http://localhost:3333')
+console.log(env.DATABASE_URL)
+
+server.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
+  console.log(`Server is running on http://localhost:${env.PORT}`)
 })
